579a3b6478e443210d9c60d06c5113338260b1f1
Marcel <mk@script-base.com>
2013-10-25 17:06:01 +0200

    basic HTML5 Client

---
+++ www/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Multi-Chat</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="root" >
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="css/chat.css" rel="stylesheet">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="ico/favicon.png">
+    <?php if($_GET['url']=="chat") { print '<script language="javascript" type="text/javascript" src="js/chat.js"></script>'; } ?>
+    
+    
+    <script>var c = new chat('inp','chat');</script>
+    </head>
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">Multi-Chat</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="index.php">Home</a></li>
+              <li><a href="index.php?url=chat">Chat</a></li>
+              <li><a href="index.php?url=about">About</a></li>
+              
+            </ul>
+				 <form class="navbar-form pull-right">
+              <input class="span2" type="text" placeholder="Email">
+             
+              <input class="span2" type="password" placeholder="Password">
+              <button type="submit" class="btn">Sign in</button>  <button type="submit" class="btn">Register</button>
+            </form>          
+            </form>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+      
+      
+      <?php
+				  switch($_GET['url']) {      
+				    case ("chat"): include("content/chat.php"); break;
+				    default: include("content/home.php"); 
+					};           
+				?>
+      </div>
+
+     
+
+      <hr>
+
+      <footer>
+        <p>&copy; script-base[.]com 2013</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+    <script src="js/bootstrap-alert.js"></script>
+    <script src="js/bootstrap-modal.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/bootstrap-scrollspy.js"></script>
+    <script src="js/bootstrap-tab.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/bootstrap-popover.js"></script>
+    <script src="js/bootstrap-button.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+    <script src="js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
---
+++ www/js/chat.js
@@ -0,0 +1,71 @@
+function chat(inp,out) 
+{
+	var ws = null;
+	var input = inp;
+	var output = out;
+
+
+
+
+	this.connect = function ()
+	{
+	  if ("WebSocket" in window)
+	  {
+	     
+	     // Let us open a web socket
+	     ws = new WebSocket("ws://localhost:9998", ['base64']);
+	     ws.onopen = function()
+	     {
+	        // Web Socket is connected, send data using send()
+	       input = document.getElementById(input);
+	       output = document.getElementById(output);
+	       
+	       
+	     };
+	     ws.onmessage = function (evt) 
+	     { 
+	       var received_msg = evt.data;
+	       
+	   	 output.innerHTML  +=decodeURIComponent(escape(window.atob( received_msg )))+'<br>';
+	   		
+	       output.scrollTop = output.scrollHeight;
+	       // document.getElementById('chat').value
+	     };
+	     ws.onclose = function()
+	     { 
+	        // websocket is closed.
+	        alert("Connection is closed..."); 
+	     };
+	     
+	  }
+	  else
+	  {
+	     // The browser doesn't support WebSocket
+	     alert("WebSocket NOT supported by your Browser!");
+	  }
+	 };
+	 
+	 this.loginGuest = function () {
+	 	this.sendDirectly("['/login','guest']");
+	 };
+	 
+	 this.sendDirectly = function (msg) {
+	 	 ws.send(window.btoa(unescape(encodeURIComponent( msg ))));
+	 	};
+	 
+	 this.sendmsg = function () {
+	  		var t = input.value.replace(/(<([^>]+)>)/ig,"");
+	  		if (t.length<1) return false;
+	  		input.value ="";
+	  		t=  t.replace(/\'/g,"\\'");
+			this.sendDirectly("['/send','Welcome hall','"+t+"']");
+			
+			// document.getElementById('inp').value
+		};
+		
+	this.disconnect = function () {
+	  	
+			ws.send("['/logout','me']");
+		};
+
+}
